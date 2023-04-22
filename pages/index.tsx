6665e7eabf1790e0aa2d283496b97a14d645a026
012xx <night.fox.045@gmail.com>
2023-04-22 15:14:27 +0000

    :sparkles: : サンプルデータ表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import { siteConfig } from "@/site.config";
+import { sampleCards } from "@/utils/sample";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,9 @@ const Home: NextPage = () => {
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
+          {sampleCards.map((page, index) => (
+            <p key={index}>{page.name}</p>
+          ))}
         </div>
       </div>
     </Layout>
