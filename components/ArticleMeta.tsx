ed1fa4bb076ba25ea85ee5ad0e3084a5fde7edb4
012xx <night.fox.045@gmail.com>
2023-04-24 09:07:58 +0000

    :recycle: : props & 型定義

--- components/ArticleMeta.tsx
+++ components/ArticleMeta.tsx
@@ -1,13 +1,14 @@
+import { ArticleMetaProps } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const ArticleMeta = () => {
+const ArticleMeta = ({ page }: ArticleMetaProps) => {
   return (
     <>
       {/* page cover */}
       <Image
         className="w-full static h-auto"
-        src={"/"}
+        src={page.cover}
         alt=""
         width={640}
         height={360}
@@ -15,22 +16,24 @@ const ArticleMeta = () => {
       />
 
       {/* page name */}
-      <h1 className="my-8">page name</h1>
+      <h1 className="my-8">{page.name}</h1>
       <div className="bg-gray-100 px-6 py-4 rounded text-sm text-gray-500">
         <div className="grid grid-cols-3 gap-4">
           {/* published */}
           <div className="col-span-1">Published</div>
-          <div className="col-span-2">published</div>
+          <div className="col-span-2">{page.published}</div>
 
           {/* author */}
           <div className="col-span-1">Author</div>
-          <div className="col-span-2">author</div>
+          <div className="col-span-2">{page.author}</div>
 
           {/* tags */}
           <div className="col-span-1">Tags</div>
           <div className="col-span-2">
             {/* change later */}
-            <span>#tag</span>
+            {page.tags.map((tag: string) => (
+              <span key={tag}>{`#${tag} `}</span>
+            ))}
           </div>
         </div>
       </div>
