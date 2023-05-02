d9a526878bbec7443cb405fc1c25b6cf3ed65552
012xx <night.fox.045@gmail.com>
2023-05-02 17:10:24 +0000

    :sparkles: : SSR対応

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,35 +1,10 @@
-import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 import { Params, TagProps } from "../../types/types";
 import { fetchPages } from "../../utils/notion";
-import { getMultiSelect } from "../../utils/property";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { results }: { results: Record<string, any>[] } = await fetchPages({});
-
-  const pathSet: Set<string> = new Set();
-  for (const page of results) {
-    for (const tag of getMultiSelect(page.properties.tags.multi_select)) {
-      pathSet.add(tag);
-    }
-  }
-
-  const paths = Array.from(pathSet).map((tag) => {
-    return {
-      params: {
-        tag: tag,
-      },
-    };
-  });
-
-  return {
-    paths: paths,
-    fallback: "blocking",
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { tag } = ctx.params as Params;
   const { results } = await fetchPages({ tag: tag });
   return {
@@ -37,7 +12,6 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       pages: results ? results : [],
       tag: tag,
     },
-    revalidate: 10,
   };
 };
 
